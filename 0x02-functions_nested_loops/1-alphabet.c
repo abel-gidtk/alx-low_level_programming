aaa921e7c3a8e6b6c5ad7ee23d72cec0ac28f226
Abel Gideon <abelgideontk@gmail.com>
2022-12-13 15:49:24 +0300

    Add alphabet file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+/**
+ * print_alphabet - Prints the alphabet in lowercase
+ *
+ * Return: void
+*/
+void print_alphabet(void)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
