6cfe9bcb06da6600c21182d76a38442230d46b7f
Abel Gideon <abelgideontk@gmail.com>
2022-12-13 17:01:27 +0300

    Fix returns and add prints

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,9 +9,18 @@
 int print_sign(int n)
 {
 	if (n < 0)
+	{
+		_putchar(45);
 		return (-1);
+	}
 	else if (n > 0)
-		return (+1);
+	{
+		_putchar(43);
+		return (1);
+	}
 	else
+	{
+		_puthcar(48);
 		return (0);
+	}
 }
