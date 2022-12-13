d088a40f9d413697c51c6804adb0e99a78e2861b
Abel Gideon <abelgideontk@gmail.com>
2022-12-13 16:18:55 +0300

    Add print alphabet_x10 file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the alphabet 10 times
+ *
+ * Return: void
+*/
+void print_alphabet_x10(void)
+{
+	int i;
+	int j = 1;
+
+	while (j <= 10)
+	{
+		i = 97;
+		while (i < 123)
+		{
+			_putchar(i);
+			i++;
+		}
+	}
+	_putchar('\n');
+}
