652877ddaf1b0dc010e794a7349fe35b4f373de8
Abel Gideon <abelgideontk@gmail.com>
2022-12-15 18:02:57 +0300

    Add fizz buzz file

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Prints 1 to 100 but for multiplies of 3 prints Fizz
+ * for multiplies of 5 prints Buzz
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else
+			printf("%d ", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
+
