6c80fc0f3468f1249948fe7e5a39e98b0da69712
Abel Gideon <abelgideontk@gmail.com>
2022-12-15 13:31:44 +0300

    Hot fix 2

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,7 +12,9 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
+	int is_leap_year = (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
+
+	if (is_leap_year)
 	{
 		if (month >= 2 && day >= 60)
 		{
