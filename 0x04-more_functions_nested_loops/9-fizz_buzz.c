a6e6bfbb440b3688b9bbb215c4bec26ccfc69ef8
Abel Gideon <abelgideontk@gmail.com>
2022-12-15 18:12:50 +0300

    Remove extra space at the end of Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,14 +15,17 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
-		else if (i % 5 == 0)
+		else if (i % 5 == 0 && i != 100)
 			printf("Buzz ");
+		else if (i % 5 == 0 && i == 100)
+			printf("Buzz");
 		else if (i % 3 == 0)
 			printf("Fizz ");
 		else
 			printf("%d ", i);
 		i++;
 	}
+	printf("\n");
 	return (0);
 }
 
