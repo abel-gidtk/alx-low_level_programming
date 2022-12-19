04c0e926c7dd76882693b1d26ff37d5c462eaefc
Abel Gideon <abelgideontk@gmail.com>
2022-12-19 15:47:00 +0300

    Add rev string file

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string
+ * @s: String to be reversed
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+	int count;
+
+	while (s[i] != 0)
+	{
+		count++;
+		i++;
+	}
+}
