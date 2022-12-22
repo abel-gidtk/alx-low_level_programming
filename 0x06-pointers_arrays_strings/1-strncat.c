5c3b92784d15496dbedfd4edec8ee8e9b3f8c462
Abel Gideon <abelgideontk@gmail.com>
2022-12-22 13:05:37 +0300

    Add strncat file

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - Concatenates two strings
+ * @dest: Destination
+ * @src: Source
+ * @n: Number of bytes
+ *
+ * Return: Resulting string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int len1 = strlen(dest);
+	int len2 = strlen(src);
+
+	while (i < n && i < len2)
+	{
+		*(dest + len1 + i) = src[i];
+		i++;
+	}
+	return (dest);
+}
