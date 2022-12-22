22b8b468681eaf5084e26a24a3b4197af2d53d4b
Abel Gideon <abelgideontk@gmail.com>
2022-12-22 13:47:22 +0300

    Add strcmp file

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - Compares two strings
+ * @s1: String 1
+ * @s2: String 2
+ *
+ * Return: Difference in ASCII value
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int diff;
+
+	diff = *s1 - *s2;
+	return (diff);
+}
