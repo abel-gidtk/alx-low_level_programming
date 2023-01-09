040758345471f45cb36b104925af4edf58171230
Abel Gideon <abelgideontk@gmail.com>
2023-01-09 12:50:19 +0300

    Add alloc grid file

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - Pointer to a 2D array of ints
+ * @width: Width of array
+ * @height: Height of array
+ *
+ * Return: Pointer to 2D array NULL if fail
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i, j, k;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	grid = malloc(sizeof(int *) * height);
+	if (grid == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+			return (NULL);
+	}
+	for (j = 0; j < height; j++)
+	{
+		for (k = 0; k < width; k++)
+			grid[j][k] = 0;
+	}
+	return (grid);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,5 +4,6 @@
 char *create_array(unsigned int, char);
 char *_strdup(char *);
 char *str_concat(char *, char *);
+int **alloc_grd(int, int);
 
 #endif
