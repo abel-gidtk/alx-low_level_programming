567817f7904a5dbca49dca453f256404ec502ba3
Abel Gideon <abelgideontk@gmail.com>
2023-01-12 23:56:10 +0300

    Gave malloc No. of elem instead of No. of bytes

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,19 +12,17 @@
 int *array_range(int min, int max)
 {
 	int *ptr;
-	int size, j, i;
+	int size, i;
 
 	if (min > max)
 		return (NULL);
 	size = (max - min) + 1;
-	ptr = malloc(size);
+	ptr = malloc(size * sizeof(int));
 	if (ptr == NULL)
 		return (NULL);
-	j = min;
 	for (i = 0; i < size; i++)
 	{
-		ptr[i] = j;
-		j++;
+		ptr[i] = min++;
 	}
 	return (ptr);
 }
