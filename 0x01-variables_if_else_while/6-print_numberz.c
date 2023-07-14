a3298ae2224001857e876ce322f372aef67d5889
Abel Gideon <abelgdtk.coding@gmail.com>
2023-07-14 15:53:19 +0300

    Add 6-print_numberz.c file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print numbers
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
