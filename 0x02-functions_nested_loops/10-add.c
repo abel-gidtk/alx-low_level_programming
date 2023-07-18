78dd0c41bb6e72759db019e00158f840613b22a4
Abel Gideon <abelgdtk.coding@gmail.com>
2023-07-18 13:02:02 +0300

    Remove space in function decleration

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - Adds two integers
+ * @n1: First integer to be added
+ * @n2: Second integer to be added
+ *
+ * Return: Sum of n1 and n2
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
