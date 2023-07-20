c895b58379b98664d7191e3678c2756b20eaa90f
Abel Gideon <abelgdtk.coding@gmail.com>
2023-07-20 09:59:52 +0300

    Add 9-fizz_buzz.c file

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Prints 1 to 100 replacing multiplies of 5 and 3
+ *
+ * Return: 1 for success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		if (i != 100)
+			printf(" ");
+	}
+	printf("\n");
+}
