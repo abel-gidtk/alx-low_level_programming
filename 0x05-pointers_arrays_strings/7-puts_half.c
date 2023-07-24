03dcc9c661886089935062d41dfcb43257974baf
Abel Gideon <abelgdtk.coding@gmail.com>
2023-07-24 14:24:57 +0300

    Add 7-puts_half.c file

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - Prints half of a string
+ * @str: String to be printed
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i;
+	int length = strlen(str);
+
+	if (length % 2 == 0)
+	{
+		for (i = length / 2; i < length; i++)
+			_putchar(str[i]);
+	}
+	else
+	{
+		for (i = (length - 1) / 2; i < length; i++)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
