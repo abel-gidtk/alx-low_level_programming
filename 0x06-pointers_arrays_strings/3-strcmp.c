ee7e1bba92307d1bf6818b79b9e5aa220a5e9bf4
Abel Gideon <abelgdtk.coding@gmail.com>
2023-07-28 12:44:17 +0300

    Add 3-strcmp.c file

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - Compares two strings
+ * @s1: String 1
+ * @s2: String 2
+ *
+ * Retrun: 0 if equal 15 if s1 > s2 -15 if s2 > s1
+ */
+int _strcmp(char *s1, char *s2)
+{
+	if (s1 == s2)
+		return (0);
+	else if (s1 > s2)
+		return (15);
+	return (-15);
+}
