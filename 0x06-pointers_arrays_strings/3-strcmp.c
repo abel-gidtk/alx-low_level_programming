b620be1e4c35e28b856928fa29b499ef216ed698
Abel Gideon <abelgdtk.coding@gmail.com>
2023-07-28 12:45:05 +0300

    Fix betty errors

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,7 +6,7 @@
  * @s1: String 1
  * @s2: String 2
  *
- * Retrun: 0 if equal 15 if s1 > s2 -15 if s2 > s1
+ * Return: 0 if equal 15 if s1 > s2 -15 if s2 > s1
  */
 int _strcmp(char *s1, char *s2)
 {
