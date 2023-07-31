251d4f04cdb67daafeda3b500fd1c86afbc29cad
Abel Gideon <abelgdtk.coding@gmail.com>
2023-07-31 15:27:15 +0300

    Add 2-strchr.c file

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - Locates a character in a string
+ * @s: Pointer to the string
+ * @c: Character to find
+ *
+ * Return: Pointer to c
+ */
+char *_strchr(char *s, char c)
+{
+	char *ptr;
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			ptr = &s[i];
+			return (ptr);
+		}
+		i++;
+	}
+	return (NULL);
+}
