ce9d7880c1984b57002330e74daf4f537da2f0eb
Abel Gideon <abelgdtk.coding@gmail.com>
2023-08-02 19:57:17 +0300

    Add 5-sqrt_recursion.c file

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - Returns the natural square root of a number
+ * @n: Number
+ *
+ * Return: Square root
+ */
+int _sqrt_recursion(int n)
+{
+	_putchar(n);
+	return (1);
+}
