82f18a47e8f2184511609d53b5a6d0939ef07476
Abel Gideon <abelgdtk.coding@gmail.com>
2023-08-14 14:07:04 +0300

    Add dog.h file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,12 +9,12 @@
  *
  * Description: Structure for a dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *, char *, float, char *);
 void print_dog(struct dog *);
