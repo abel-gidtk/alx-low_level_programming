02489c0ae0a8e1be5c41dbd60a9afcabce95f7ec
Abel Gideon <abelgdtk.coding@gmail.com>
2023-08-16 19:49:50 +0300

    Add function_pointers.h file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,9 @@
 #ifndef FUNC_PTR
 #define FUNC_PTR
+#include <stddef.h>
 
 int _putchar(char);
 void print_name(char *, void (*f)(char *));
+void array_iterator(int *, size_t, void(*action)(int));
 
 #endif
