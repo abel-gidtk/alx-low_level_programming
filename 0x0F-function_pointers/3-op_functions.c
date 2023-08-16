f6ac9ef0450005e58f4c0c3a4fa30871287a7b20
Abel Gideon <abelgdtk.coding@gmail.com>
2023-08-16 20:56:55 +0300

    Add 3-op_functions.c file

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Adds two numbers
+ * @a: num1
+ * @b: num2
+ *
+ * Return: Sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Subtracts two numbers
+ * @a: num1
+ * @b: num2
+ *
+ * Return: Difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Multiplies two numbers
+ * @a: num1
+ * @b: num2
+ *
+ * Return: Product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Divides two numbers
+ * @a: num1
+ * @b: num2
+ *
+ * Return: Quotient
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Remainder of division
+ * @a: num1
+ * @b: num2
+ *
+ * Return: Mod
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
