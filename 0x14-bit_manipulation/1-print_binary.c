b3750b91942ee68d21d1370072029bb23a73560e
Abel Gideon <abelgdtk.coding@gmail.com>
2023-08-31 14:22:23 +0300

    Add even more bits

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -11,7 +11,7 @@ void print_binary(unsigned long int n)
 	unsigned long int mask;
 	int print = 0;
 
-	for (mask = 0x80000000000; mask != 0; mask >>= 1)
+	for (mask = 0x8000000000000000; mask != 0; mask >>= 1)
 	{
 		if (mask & n)
 		{
