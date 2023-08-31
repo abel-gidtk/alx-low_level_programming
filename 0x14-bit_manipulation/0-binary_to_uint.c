d7fb385993d42faffcc8dabf92f9f38728d66402
Abel Gideon <abelgdtk.coding@gmail.com>
2023-08-31 11:01:45 +0300

    Add 0-binary_to_uint.c file

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * binary_to_uint - Converts binary to int
+ * @b: String conataining binary
+ *
+ * Return: Converted int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int j, i = 0, length = strlen(b);
+	unsigned int binary = 0;
+
+	if (b == NULL)
+		return (0);
+	while (b[i] != '\0')
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		i++;
+	}
+
+	for (j = 0, i = length - 1; i >= 0; j++, i--)
+	{
+		if (b[i] == '1')
+			binary += 1 << j;
+	}
+	return (binary);
+}
