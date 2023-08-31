df11d8bdd1af4d3cb3508a61427f2fa5c322529f
Abel Gideon <abelgdtk.coding@gmail.com>
2023-08-31 15:56:34 +0300

    Fix Betty Errors

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -15,8 +15,8 @@ int get_bit(unsigned long int n, unsigned int index)
 
 	if (index >= sizeof(unsigned long int) * 8)
 	{
-		return -1;
+		return (-1);
 	}
 	mask = 1UL << index;
-	return (n & mask) ? 1 : 0;	
+	return ((n & mask) ? 1 : 0);
 }
