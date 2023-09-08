f3ccff6531a41d988761fb44292c10866ea61f80
Abel Gideon <abelgdtk.coding@gmail.com>
2023-09-08 14:47:54 +0300

    Add main.h file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,7 +3,10 @@
 #include <stddef.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 ssize_t read_textfile(const char *, size_t);
+int create_file(const char *, char *);
 
 #endif
